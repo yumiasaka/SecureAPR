58f3f2e571c4a799fe89b28e3a011e7d872af823
yumiasaka <cstatjni3801@outlook.com>
2025-12-26 12:10:12 +0800

    Wire hardhat config for SecureAPR + Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,16 +6,19 @@ import "@typechain/hardhat";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
-import { vars } from "hardhat/config";
 import "solidity-coverage";
+import * as dotenv from "dotenv";
 
 import "./tasks/accounts";
 import "./tasks/FHECounter";
+import "./tasks/SecureAPR";
+import "./tasks/sync-ui";
 
-// Run 'npx hardhat vars setup' to see the list of variables that need to be set
+dotenv.config();
 
-const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const INFURA_API_KEY = process.env.INFURA_API_KEY ?? "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY ?? "";
+const FALLBACK_PRIVATE_KEY_FOR_LOCAL_BUILDS = "0x" + "11".repeat(32);
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -24,7 +27,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      sepolia: vars.get("ETHERSCAN_API_KEY", ""),
+      sepolia: process.env.ETHERSCAN_API_KEY ?? "",
     },
   },
   gasReporter: {
@@ -34,28 +37,16 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {
-      accounts: {
-        mnemonic: MNEMONIC,
-      },
       chainId: 31337,
     },
     anvil: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
       chainId: 31337,
       url: "http://localhost:8545",
     },
     sepolia: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
       chainId: 11155111,
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY || FALLBACK_PRIVATE_KEY_FOR_LOCAL_BUILDS],
     },
   },
   paths: {
